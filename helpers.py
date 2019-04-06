1d03091b101f3c9fa2011d2c7507b75bef63f8cb
Nick H <manifes7o@gmail.com>
2019-04-06 18:21:17 -0400

    Add load_one_game for neater plot_whole_game graphs

--- helpers.py
+++ helpers.py
@@ -1,6 +1,7 @@
 import os
 from itertools import islice
 
+import matplotlib.pyplot as plt
 import pandas as pd
 
 
@@ -11,7 +12,7 @@ def load_whole_games(num_files=None):
     for i, file in islice(enumerate(files), num_files):
         with open('data/' + file, 'r') as f:
             df = pd.read_csv(f, delimiter=' ', header=None,
-                             names=['num_a', 'num_b', 'num_aces_a', 'num_aces_b', 
+                             names=['num_a', 'num_b', 'num_aces_a', 'num_aces_b',
                                     'num_kings_a', 'num_kings_b', 'win_first_round', 'wars'],
                              na_values='None')
             df['game'] = i
@@ -22,7 +23,7 @@ def load_whole_games(num_files=None):
     df['game'] = pd.to_numeric(df['game'])
 
     del df['win_first_round']
-    
+
     return df
 
 
@@ -30,9 +31,9 @@ def load_whole_games(num_files=None):
 def parse_header_footer(header, footer):
     _, _, aces, _, kings, *_ = header.split()
     num_a, num_b, *_ = footer.split()
-    
+
     a_won = int(num_a) > int(num_b)
-    
+
     return (int(aces), int(kings), a_won)
 
 
@@ -44,7 +45,7 @@ def get_game_summaries(num_files=None):
     for i, file in islice(enumerate(files), num_files):
         with open('data/' + file, 'rb') as f:
             first = f.readline()
-            
+
             ## pesky bit of code to get a_won_first_round
             found_val = False
             while not found_val:
@@ -61,16 +62,16 @@ def get_game_summaries(num_files=None):
                 except IndexError:
                     a_won_first = a > b
                     break
-                    
-            
+
+
             f.seek(-2, os.SEEK_END)
             while f.read(1) != b"\n":
                 f.seek(-2, os.SEEK_CUR)
             last = f.readline()
-            
+
             result = parse_header_footer(first, last)
             li.append([i, *result, a_won_first])
-            
+
     df = pd.DataFrame(li, columns=['game', 'a_starting_aces',
                                    'a_starting_kings',
                                    'a_won', 'a_won_first_round'])
@@ -93,8 +94,21 @@ def get_game_lengths(num_files=None):
     return df
 
 
+
+def load_one_game(game_number):
+    with open('data/data' + str(game_number) + '.txt') as f:
+        df = pd.read_csv(f, delimiter=' ', header=None,
+                         names=['num_a', 'num_b', 'num_aces_a', 'num_aces_b',
+                                'num_kings_a', 'num_kings_b',
+                                'win_first_round', 'wars'],
+                         na_values='None')
+        del df['win_first_round']
+
+    return df
+
+
 def plot_game_history(game_number):
-    game = whole_games[whole_games['game'] == game_number].reset_index()
+    game = load_one_game(game_number).reset_index()
     wars = game[game['wars'] == 1].index
 
     fig, ax = plt.subplots(figsize=(18, 10))
@@ -103,7 +117,7 @@ def plot_game_history(game_number):
     ax2 = ax.twinx()
     loc = matplotlib.ticker.MultipleLocator(1)
     ax2.yaxis.set_major_locator(loc)
-    
+
     ax2.plot(game['num_aces_a'], linewidth=3, c='g', label='Aces')
     ax2.plot(game['num_kings_a'], linewidth=1, c='k', label='Kings')
 
@@ -112,10 +126,10 @@ def plot_game_history(game_number):
 
     for war in islice(wars, 1):
         ax2.axvline(war, color='red', alpha=.5, dashes=(1, 1), label='WAR!')
-        
+
     ax.set_ylabel('Card Count', fontsize=12)
     ax.set_xlabel('Turn Count', fontsize=16)
     ax2.set_ylabel('King/Ace Count', fontsize=12)
-        
+
     ax2.legend(fontsize=16, loc=3)
     return ax2
